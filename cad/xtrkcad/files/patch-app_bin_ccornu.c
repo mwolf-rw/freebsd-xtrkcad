d56b57191cb44de6163be9b976c77d3e6ed6cd2c
Matthias Wolf <matthias@rheinwolf.de>
2018-12-28 00:34:51 +0100

    Initial commit, v5.1.2a

---
+++ cad/xtrkcad/files/patch-app_bin_ccornu.c
@@ -0,0 +1,10 @@
+--- app/bin/ccornu.c.orig	2018-10-04 18:29:16.000000000 +0200
++++ app/bin/ccornu.c	2018-12-27 23:21:31.684550000 +0100
+@@ -64,6 +64,7 @@
+ 
+ 
+ #include "track.h"
++#include "bezctx.h"
+ #include "spiro.h"
+ #include "spiroentrypoints.h"
+ #include "bezctx_xtrkcad.h"
