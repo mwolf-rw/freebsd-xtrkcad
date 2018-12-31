b2cbd6fbe94c54ba3799fd964c26e15a282fe1b0
Matthias Wolf <matthias@rheinwolf.de>
2018-12-31 03:15:48 +0100

    Make portlint happy

--- cad/xtrkcad/files/patch-app_bin_ccornu.c
+++ cad/xtrkcad/files/patch-app_bin_ccornu.c
@@ -1,5 +1,5 @@
---- app/bin/ccornu.c.orig	2018-10-04 18:29:16.000000000 +0200
-+++ app/bin/ccornu.c	2018-12-27 23:21:31.684550000 +0100
+--- app/bin/ccornu.c.orig	2018-10-04 16:29:16 UTC
++++ app/bin/ccornu.c
 @@ -64,6 +64,7 @@
  
  
